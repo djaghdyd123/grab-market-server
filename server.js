41566395ee9d05646f60813b86c4174c115c73c1
eomholyong <eomholyong@eomholyong-ui-MacBookPro.local>
2021-03-30 23:22:12 +0900

    Add search & upload API

--- server.js
+++ server.js
@@ -8,42 +8,63 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/products", (req, res) => {
-  const query = req.query;
-  console.log("QUERY", query);
-  res.send({
-    products: [
-      {
-        id: 1,
-        name: "농구공",
-        price: 100000,
-        seller: "조던",
-        imageUrl: "images/products/basketball1.jpeg",
-      },
-      {
-        id: 2,
-        name: "축구공",
-        price: 50000,
-        seller: "메시",
-        imageUrl: "images/products/soccerball1.jpg",
-      },
-      {
-        id: 3,
-        name: "키보드",
-        price: 10000,
-        seller: "그랩",
-        imageUrl: "images/products/keyboard1.jpg",
-      },
-    ],
-  });
+  models.Product.findAll({
+    //limit : 2
+    order: [["createdAt", "DESC"]],
+    attribute: ["id", "name", "price", "createdAt", "seller"],
+  })
+    .then((result) => {
+      console.log("PRODUCT : ", result);
+      res.send({
+        products: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("error!");
+    });
 });
 
 app.post("/products", (req, res) => {
   const body = req.body;
-  res.send({
-    body,
-  });
+  const { name, description, price, seller } = body;
+  if (!name || !description || !price || !seller) {
+    res.send("모든 필드를 입력해주세요");
+  }
+  models.Product.create({
+    name,
+    description,
+    price,
+    seller,
+  })
+    .then((result) => {
+      console.log("상품 생성결과 :", result);
+      res.send({
+        result,
+      });
+    })
+    .catch((error) => {
+      console.log("에러가 발생하였습니다.");
+    });
 });
-
+app.get("/products/:id", (req, res) => {
+  const params = req.params;
+  const { id } = params;
+  models.Product.findOne({
+    where: {
+      id: id,
+    },
+  })
+    .then((result) => {
+      console.log("PRODUCT : ", result);
+      res.send({
+        product: result,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 app.listen(port, () => {
   console.log("그랩의 서버가 돌아가고 있습니다.");
   models.sequelize
